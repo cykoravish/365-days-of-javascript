36cdc3db18d1a6f67d98b19b54dbcafc17fae3bf
cykoravish <ravishbisht85@gmail.com>
2026-01-01 07:42:57 +0530

    add day 1 question

---
+++ day001.js
@@ -0,0 +1,26 @@
+// 1. Write a JavaScript function that takes a person’s name and returns this message:
+
+//"Happy New Year 2026, <name>! 🎉"
+
+/*
+// method - 1 (using template literals)
+function wishNewYear(name) {
+  return `Happy New Year 2026, ${name}! 🎉`;
+}
+console.log(wishNewYear("Khizra"));
+*/
+
+/*
+// method - 2 (using string concatination)
+function wishNewYear(name){
+    return "Happy New Year 2026 " + name + "! 🎉";
+}
+console.log(wishNewYear("Khizra"));
+*/
+
+/*
+//using arrow function
+const wishNewYear = (name) => `Happy New Year 2026, ${name}! 🎉`;
+console.log(wishNewYear("Khizra"));
+*/
+
