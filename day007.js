6757023538d3c24bfa59ee2d0e4f3ec1bae135cf
cykoravish <ravishbisht85@gmail.com>
2026-01-06 23:00:25 +0530

    day-7 question add

---
+++ day007.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program that takes a number and checks whether it is a prime number or not.
+
+// Example:
+
+// Input → 7 → Output → Prime number
+
+// Input → 10 → Output → Not a prime number
