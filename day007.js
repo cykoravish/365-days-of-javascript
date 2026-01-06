6f6d3b4ec141106f0c59d1b7ca24255aed5bfd47
cykoravish <ravishbisht85@gmail.com>
2026-01-06 23:06:41 +0530

    add prime number checker function

--- day007.js
+++ day007.js
@@ -5,3 +5,15 @@
 // Input → 7 → Output → Prime number
 
 // Input → 10 → Output → Not a prime number
+
+function isPrime(num) {
+  if (num <= 1) return false;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(7));  // true
+console.log(isPrime(10)); // false
