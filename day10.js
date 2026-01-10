30c29e2fa64fe0a491e29ed0ec7a71b2cf3a1605
cykoravish <ravishbisht85@gmail.com>
2026-01-10 06:21:11 +0530

    find sum of n natural number add

---
+++ day10.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program that takes a number and prints the sum of all natural numbers from 1 to that number.
+
+// Example:
+
+// Input → 5
+
+// Output → 15 (1 + 2 + 3 + 4 + 5)
+
+// function findSum(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+// console.log(findSum(5));
