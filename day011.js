a8fa6d6848766c58fe8c835c65b981c464a420bf
cykoravish <ravishbisht85@gmail.com>
2026-01-11 18:38:20 +0530

    day 11 ques add

---
+++ day011.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program that takes a string and checks whether it contains only numbers.
+
+// Example:
+
+// Input → "12345" → Output → Only numbers
+
+// Input → "12a3" → Output → Contains non-numeric characters
