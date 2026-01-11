87df8ddf78a625c32d43599fcdc35b8572e11d28
cykoravish <ravishbisht85@gmail.com>
2026-01-11 18:43:20 +0530

    solving day 11 ques

--- day011.js
+++ day011.js
@@ -5,3 +5,13 @@
 // Input → "12345" → Output → Only numbers
 
 // Input → "12a3" → Output → Contains non-numeric characters
+
+function checkString(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      console.log();
+    } else {
+      console.log();
+    }
+  }
+}
