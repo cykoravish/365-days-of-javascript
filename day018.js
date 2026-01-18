072fcb51954cc40cb2442c019d05951383279e8d
cykoravish <ravishbisht85@gmail.com>
2026-01-18 12:11:57 +0530

    check string characters are unique

---
+++ day018.js
@@ -0,0 +1,26 @@
+// Write a JavaScript program that takes a string and checks whether all characters are unique (no repeated characters).
+
+// Example:
+
+// Input → "abcde" → Output → All characters are unique
+
+// Input → "hello" → Output → Duplicate characters found
+
+function checkUniqueChar(str) {
+  let strArr = str.split("");
+  let strFreq = {};
+  strArr.forEach((e) => {
+    if (strFreq[e]) {
+      strFreq[e] += 1;
+    } else {
+      strFreq[e] = 1;
+    }
+  });
+  for (let key in strFreq) {
+    if (strFreq[key] > 1) {
+      return "Duplicate characters found";
+    }
+  }
+  return "All characters are unique";
+}
+console.log(checkUniqueChar("ravisha"));
