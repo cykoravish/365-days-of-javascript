c6c347b2b467574d5d4c0f13ee38a7506d1328e2
cykoravish <ravishbisht85@gmail.com>
2026-02-14 06:27:16 +0530

    object definition add

---
+++ day043.js
@@ -0,0 +1,3 @@
+// Q. What is an object in JavaScript?
+
+// An object in JavaScript is a collection of key–value pairs where values can be data (properties) or functions (methods) used to represent a real-world entity.
